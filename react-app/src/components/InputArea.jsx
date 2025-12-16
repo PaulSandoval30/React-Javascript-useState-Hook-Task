c46e88fd8ff726866fe42489855d6859e0da92fc
Paul Sandoval <sunghayuki30@gmail.com>
2025-12-16 12:16:14 +0800

    add files

---
+++ react-app/src/components/InputArea.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import '../CSS/InputArea.css'
+
+
+function InputArea({ addTodo }) {
+  const [task, setTask] = useState("");
+
+  const handleAddTodo = () => {
+    addTodo(task);
+    setTask("");
+    
+  };
+  return (
+    <>
+     <div className="input-panel">
+        <h2>Add a new task</h2>
+        <div className="input-group">
+          <input type="text" value={task} onChange={(e) => setTask(e.target.value)} placeholder='Enter task here...' />
+        </div>
+        <button className='add-task-btn' onClick={handleAddTodo}>Add Task</button>
+      </div>
+    </>
+  )
+}
+
+export default InputArea
---
+++ react-app/src/components/ListArea.jsx
@@ -0,0 +1,42 @@
+import "../CSS/listArea.css";
+
+function ListArea({ todos, setTodos }) {
+  const deleteTodo = (index) => {
+    const newTodos = [];
+  
+    for (let i = 0; i < todos.length; i++) {
+      if (i !== index) {
+        newTodos.push(todos[i]);
+      }
+    }
+  
+    setTodos(newTodos);
+  };
+  return (
+    <>
+      <div className="list-panel">
+        {todos.map((todo, index) => (
+          <div className="task" key={index}>
+            <div className="checkbox-list-wrapper">
+              <input type="checkbox" id={`checkbox-${index}`} />
+              <label htmlFor={`checkbox-${index}`} className="custom-checkbox">
+                <ion-icon name="checkmark-outline"></ion-icon>
+              </label>
+              <label htmlFor={`checkbox-${index}`} className="task-name">
+                {todo}
+              </label>
+            </div>
+            <button
+              className="delete-task-btn"
+              onClick={() => deleteTodo(index)}
+            >
+              <ion-icon name="trash-outline"></ion-icon>
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default ListArea;
